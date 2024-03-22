75ad9a3050ad625db022f50f8f1f42227c78b435
maisiewalkerstelling <maisie.walkerstelling@anu.edu.au>
2024-03-22 15:38:13 +1100

    Updates: colnames displaying as SNP names not the subpops

--- popkintrial.R
+++ popkintrial.R
@@ -1,5 +1,5 @@
 #Working through popkin vignette####
-
+#want to get a kinship setup where the kinship between founders is not assumed zero
 #set working directory
 setwd("c:/userdata/walkersm/pookilapopkin")
 
@@ -13,13 +13,15 @@ library(tidyverse)
 
 #import bed matrix
 library(BEDMatrix)
-X <- BEDMatrix('NHM_plink7.bed') 
+X <- BEDMatrix('NHM_plink7') 
 # note: excluding extension is ok, as long as your bim, bed, map and fam files have the same name
 
 #This `BEDMatrix` object is not a regular matrix but `popkin` handles it correctly.
 
-#check number of individuals and SNPs are correct
+#check number of individuals and SNPs are correct. They are correct, but are somehow being read in the wrong way (Ind as no. SNPs and visaversa)
 dim(X)
 
-#mine is displaying colnames as the SNPs
+#mine is displaying colnames as the SNPs. Need to reconfigure bim file
 colnames(X)
+
+
